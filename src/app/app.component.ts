047e91ef6d4e4d45aa3148a3b85e0d7073cc0485
Praveen <42315680+praveensden@users.noreply.github.com>
2022-06-11 21:57:53 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostListener } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  @HostListener('paste', ['$event']) blockPaste(e: KeyboardEvent) {
+    e.preventDefault();
+  }
+
+  @HostListener('copy', ['$event']) blockCopy(e: KeyboardEvent) {
+    e.preventDefault();
+  }
+
+  @HostListener('cut', ['$event']) blockCut(e: KeyboardEvent) {
+    e.preventDefault();
+  }
+  randomText: string = faker.lorem.sentence();
+  enteredText: string = '';
+  OnInput(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+}
